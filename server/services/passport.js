8673976b2b67fbafef5bc46621f7c4c34dc9fffa
Fabien Berdah <fabien.berdah@gmail.com>
2021-06-01 14:01:45 +0300

    trying to fix issue with heroku

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("../config/keys");
 const mongoose = require("mongoose");
+const keys = require("../config/keys");
 
 const User = mongoose.model("users");
 
@@ -26,8 +26,10 @@ passport.use(
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then((existingUser) => {
         if (existingUser) {
+          // we already have a record with the given profile ID
           done(null, existingUser);
         } else {
+          // we don't have a user record with this ID, make a new record!
           new User({ googleId: profile.id })
             .save()
             .then((user) => done(null, user));
