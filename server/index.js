7cc65f13a11506ab085ae5b48a7eb178b04407c1
Fabien Berdah <fabien.berdah@gmail.com>
2021-06-10 13:37:26 +0300

    updated typo in front-end

--- server/index.js
+++ server/index.js
@@ -40,6 +40,19 @@ app.get("/", (req, res) => {
   res.send("Welcome to Emaily");
 });
 
+if (process.env.NODE_ENV === "production") {
+  //make sure that express will serve up production assess like
+  //main.js or main.css files
+  app.use(express.static("client/build"));
+
+  //express will serve up the index.html file if it doesn't
+  //recognize the file requested in authRoutes, billingRoutes or build
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
