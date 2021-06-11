deca7b1ca24d260297670ed8dac61ad1438eca6b
Fabien Berdah <fabien.berdah@gmail.com>
2021-06-11 06:19:22 +0300

    fix multiple errors in application

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI, {
@@ -35,6 +36,7 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   //make sure that express will serve up production assess like
---
+++ server/routes/surveyRoutes.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const requireCredits = require("../middlewares/requireCredits");
+
+const Survey = mongoose.model("surveys");
+
+module.exports = (app) => {
+  // here, before we do anything, we need to make sure that the user is logged in and  has enough credits
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipient } = req.body;
+
+    //creating an instance of the survey
+    const survey = new Survey({
+      title, //short for title: title
+      subject,
+      body,
+      // since recipient is a subdocument collection that uses the recipient schema
+      // recipients: recipients.split(',').map(email=>{ return {email: email}});
+      recipients: recipients.split(",").map((email) => ({ email })),
+    });
+  });
+};
